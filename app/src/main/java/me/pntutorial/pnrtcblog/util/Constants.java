7486221eb7463543c00d8d44120641ef852c4728
Dejan Petkovic <dejanpetko@hotmail.com>
2016-06-21 22:35:56 +0200

    init

---
+++ app/src/main/java/me/pntutorial/pnrtcblog/util/Constants.java
@@ -0,0 +1,17 @@
+package me.pntutorial.pnrtcblog.util;
+
+/**
+ * TODO: Change "demo" to your Pub and Sub Keys
+ * TODO: Create a standby suffix to differentiate SDP messages from stand by messages
+ */
+public class Constants {
+    public static final String SHARED_PREFS = "me.pntutorial.SHARED_PREFS";
+    public static final String USER_NAME    = "me.pntutorial.USERNAME";
+
+    public static final String PUB_KEY = "pub-c-3ad1182e-5e95-4726-a465-3f106374f280"; // Your Pub Key
+    public static final String SUB_KEY = "sub-c-97197a88-37e1-11e6-a63b-0619f8945a4f"; // Your Sub Key
+
+    public static final String STDBY_SUFFIX = "-stdby";
+    public static final String JSON_CALL_USER = "call_user";
+    public static final String CALL_USER = "CALL_USER";
+}
